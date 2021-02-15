936a59b55ada59c909a3b05193ceb00bf1dd33e8
Bev Antwi <akwua8@hotmail.com>
2021-02-15 16:28:22 +0000

    refined code and added consts

--- src/Components/searchBar.js
+++ src/Components/searchBar.js
@@ -1,19 +1,29 @@
-import React from 'react';
-import getTheWeather from './Components/getWeather'
+import React , {useState} from 'react';
+import {getWeather} from './getWeather.js'
 // implement some more features
 
 
-const searchBar =({query,setQuery})=> {
+const SearchBar =()=> {
 
+  const [query, setQuery] = useState('')
+
+  const retrieveResult = (event) => {
+if (event.key === 13){
+  getWeather()
+}
+  }
+
+  return (
    <input
     type="text"
     placeholder="Enter a city..."
     autoComplete="off"
     onChange={e => setQuery(e.target.value)}
-    onKeyPress={getTheWeather}
+    onKeyPress= {retrieveResult}
     value= {query}
+    
     
     />
 
-}
-export default searchBar;
+  )}
+export default SearchBar;
