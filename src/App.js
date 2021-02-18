a45987380676c551033b309958f1929d730b8893
Bev Antwi <akwua8@hotmail.com>
2021-02-18 18:19:57 +0000

    modified font, formatted the code and dynamic background

--- src/App.js
+++ src/App.js
@@ -1,113 +1,117 @@
-import React ,{useState} from 'react';
-import './App.css';
-// import SearchBar from './Components/searchBar.js';
-// import NextButton from './Components/nextWeekButton.js';
-// import Conditions from './Conditions/weatherDisplay';
+import React, { useState } from "react";
+import "./App.css";
 
-const api ={
+//A lot of code on one page! in the future we would have this be the parent component which would take in the child components upon render
 
-  key:"cc67b19dc916f461ab3e7d8d3c68bb27",
-  base:"https://api.openweathermap.org/data/2.5/"
+const api = {
+  key: "cc67b19dc916f461ab3e7d8d3c68bb27",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
 
-}
-
- function App() {
-
-  const [query, setQuery] = useState('');
-  const [weather,setWeather] =useState({});
+function App() {
+  const [query, setQuery] = useState("");
+  const [weather, setWeather] = useState({});
 
   const retrieveResult = (event) => {
-    if (event.key === "Enter"){
+    if (event.key === "Enter") {
       fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
-        .then(res=>res.json())
-        .then(result => {
-            setQuery('');
-            setWeather(result);
-            console.log(result);         
-     })
+        .then((res) => res.json())
+        .then((result) => {
+          setQuery("");
+          setWeather(result);
+          console.log(result);
+        });
     }
-      };
-      const error=()=>{
-        return(
-          function alertMessage (){
-          <div className="form-alert" role="alert">
-           alert("Please enter a city!!");
-          </div>
-          });
-      };
+  };
+  const error = () => {
+    return function alertMessage() {
+      <div className="form-alert" role="alert">
+        alert("Please enter a city!!");
+      </div>;
+    };
+  };
 
   const dateBuild = (D) => {
-    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]; 
-  
-  let day = days[D.getDay()];
-  let date = D.getDate();
-  let month = months[D.getMonth()];
-  let year = D.getFullYear();
 
-return (
- `${day} ${date} ${month} ${year}`
-)}
-  
+    let months = [ "January","February","March","April","May","June","July","August","September","October","November","December", ];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday",];
+
+    let day = days[D.getDay()];
+    let date = D.getDate();
+    let month = months[D.getMonth()];
+    let year = D.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`;
+  };
+
   return (
-    <div className = "weather-app">
-      <header className = "weather-app">
+    <div className="weather-app">
+      <header className="weather-app">
         <h3>React Weather App</h3>
       </header>
 
-      <div className={(typeof weather.main != "undefined") ? ((weather.main.temp > 16 ) ? 'app warm' : 'app' && ((weather.main.temp < 5) ? 'app snow' : 'app' )) :'app'}>
-
-{/* <div className={(typeof weather.main != "undefined") ? ((weather.main.temp < 5) ? 'app snow' : 'app') */}
-        {/* : 'app'}> */}
-
-     <main>
-       
-        <div className="search-box">
-          <div>{error? error(): "Hey! Enter a city to get started"}
-       <input
-        type="text"
-        className="searchbar"
-        placeholder="Enter a city..."
-        autoComplete="off"
-        onChange={e => setQuery(e.target.value)}
-        onKeyPress= {retrieveResult }
-        value= {query}
-        />
-        </div>
-    </div>
-
-       {/* <div className= "button"><NextButton/></div> */}
-               
-{(typeof weather.main != "undefined") ? (
-        
-          <div className="location-box">
-            <div className="main-box">
-            <div className="location">{weather.name}, {weather.sys.country}</div>
-            <div className="date">{dateBuild(new Date())}</div>
-            <div className="temp"> {Math.round(weather.main.temp)}°c</div>
-            <div className="weather">{weather.weather[0].main}</div>
-            <div className="description">{weather.weather[0].description}</div>
+      <div
+        className={
+          typeof weather.main != "undefined"
+            ? weather.main.temp > 16
+              ? "app warm"
+              : "app" && (weather.main.temp < 5 ? "app snow" : "app")
+            : "app"
+        }
+      >
+        <main>
+          <div className="search-box">
+            <div>
+              {error ? error() : "Hey! Enter a city to get started"}
+              <input
+                type="text"
+                className="searchbar"
+                placeholder="Enter a city..."
+                autoComplete="off"
+                onChange={(e) => setQuery(e.target.value)}
+                onKeyPress={retrieveResult}
+                value={query}
+              />
             </div>
-        
-      <div className = "extra-info-box">
-      <div className = "feels-like">Feels like: {weather.main.feels_like}°c </div> 
-       <div className = "humidity">Humidity: {weather.main.humidity}%</div>
-       <div className = "pressure">Pressure: {weather.main.pressure}hPa</div>
-        <div className = "wind">Wind: {weather.wind.speed}mph </div>        
-      </div>
           </div>
-    
-        
-        ) : ('')}
 
-     
-     </main>
-     <footer className= "footer">
-       <h4>Created by Jamell, Bev , Kenneth, Meltem and Haze</h4>
-     </footer>
-    </div> 
+          {/* <div className= "button"><NextButton/></div> */}
+
+          {typeof weather.main != "undefined" ? (
+            <div className="location-box">
+              <div className="main-box">
+                <div className="location">
+                  {weather.name}, {weather.sys.country}
+                </div>
+                <div className="date">{dateBuild(new Date())}</div>
+                <div className="temp"> {Math.round(weather.main.temp)}°c</div>
+                <div className="description">
+                  {weather.weather[0].description}
+                </div>
+              </div>
+
+              <div className="extra-info-box">
+                <div className="feels-like">
+                  Feels like: {weather.main.feels_like} °c{" "}
+                </div>
+                <div className="humidity">
+                  Humidity: {weather.main.humidity} %
+                </div>
+                <div className="pressure">
+                  Pressure: {weather.main.pressure} hPa
+                </div>
+                <div className="wind">Wind: {weather.wind.speed} mph </div>
+              </div>
+            </div>
+          ) : (
+            ""
+          )}
+        </main>
+        <footer className="footer">
+          <h4>Created by Jamell, Bev , Meltem, Haze and Kenneth</h4>
+        </footer>
+      </div>
     </div>
-  
-  ); 
+  );
 }
-export default App
+export default App;
